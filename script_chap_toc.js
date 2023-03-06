03289c35544d9b2b18fa56acd21b85784616c422
danielmao2019 <danielmao2019@gmail.com>
2023-03-06 11:28:59 -0500

    updates

--- script_chap_toc.js
+++ script_chap_toc.js
@@ -1,32 +1,30 @@
-// Define a function to generate the TOC
-function generateTOC() {
-  // Get the TOC container element
-  var tocContainer = document.querySelector(".chap-toc");
-
-  // Find all the h2 tags in the document
-  var headings = document.querySelectorAll("h2");
-
-  // Create an unordered list of links to the headings
-  var tocList = document.createElement("ol");
-  headings.forEach(function (heading) {
-    // Create a list item element
-    var tocListItem = document.createElement("li");
-
-    // Create an anchor element with a link to the heading ID
-    var tocLink = document.createElement("a");
-    tocLink.textContent = heading.textContent;
-    tocLink.href = "#" + heading.id;
-
-    // Add the anchor element to the list item element
-    tocListItem.appendChild(tocLink);
-
-    // Add the list item element to the TOC list
-    tocList.appendChild(tocListItem);
-  });
-
-  // Insert the TOC list into the TOC container
-  tocContainer.appendChild(tocList);
-}
-
-// Call the generateTOC function when the DOM is ready
-document.addEventListener("DOMContentLoaded", generateTOC);
+document.addEventListener('DOMContentLoaded', function() {
+    const headings = document.querySelectorAll('h2, h3');
+    var toc_list = document.createElement('ol');
+    for (let i = 0; i < headings.length; i++)
+    {
+        const heading = headings[i];
+        const level = parseInt(heading.tagName.charAt(1));
+        var list_item = document.createElement('li');
+        const section_anchor = document.createElement('a');
+        section_anchor.textContent = heading.textContent;
+        section_anchor.href = "#" + heading.id;
+        list_item.appendChild(section_anchor);
+        if (level === 2)
+        {
+            toc_list.appendChild(list_item);
+        }
+        else if (level === 3)
+        {
+            var parent_list = toc_list.lastChild.querySelector('li > ol')
+            if (parent_list === null)
+            {
+                parent_list = document.createElement('ol');
+                toc_list.lastChild.appendChild(parent_list);
+            }
+            parent_list.appendChild(list_item);
+        }
+    }
+    const container = document.querySelector('.chap-toc');
+    container.appendChild(toc_list);
+});
--- script_tex.js
+++ script_tex.js
@@ -1,11 +1,33 @@
 MathJax.Hub.Config({
-    TeX: {
-      Macros: {
-        "floor": ["\\left\\lfloor #1 \\right\\rfloor", 1],
-        "ceil": ["\\left\\lceil #1 \\right\\rceil", 1],
-        "ord": "\\operatorname{ord}",
-        "dlog": "\\operatorname{dlog}",
-        "lcm": "\\operatorname{lcm}",
-      }
+  TeX: {
+    Macros: {
+      "floor": ["\\left\\lfloor #1 \\right\\rfloor", 1],
+      "ceil": ["\\left\\lceil #1 \\right\\rceil", 1],
+      "setuni": "\\cup",
+      "setint": "\\cap",
+      "biguni": "\\bigcup",
+      "bigint": "\\bigcap",
+      // number theory
+      "ord": "\\operatorname{ord}",
+      "dlog": "\\operatorname{dlog}",
+      "lcm": "\\operatorname{lcm}",
+      "legendre": ["\\left(\\frac{#1}{#2}\\right)_{L}", 2],
+      "jacobi": ["\\left(\\frac{#1}{#2}\\right)_{J}", 2],
+      // complexity theory
+      "P": "\\mathsf{P}",
+      "NP": "\\mathsf{NP}",
+      "EXP": "\\mathsf{EXP}",
+      "time": "\\mathsf{TIME}",
+      "space": "\\mathsf{SPACE}",
+      "nspace": "\\mathsf{NSPACE}",
+      "pspace": "\\mathsf{PSPACE}",
+      "npspace": "\\mathsf{NPSPACE}",
+      "L": "\\mathsf{L}",
+      "NL": "\\mathsf{NL}",
+      "coNL": "\\mathsf{coNL}",
+      "polyl": "\\mathsf{PolyL}",
+      "poly": "\\mathsf{poly}",
+      "connected": "\\mathsf{CONNECTED}",
     }
+  }
 });
