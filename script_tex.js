7eed60bd0f16c975c9787bd7d2f53e4bea872100
danielmao2019 <danielmao2019@gmail.com>
2023-03-06 11:28:59 -0500

    updates

--- script_tex.js
+++ script_tex.js
@@ -1,11 +1,13 @@
 MathJax.Hub.Config({
-    TeX: {
-      Macros: {
-        "floor": ["\\left\\lfloor #1 \\right\\rfloor", 1],
-        "ceil": ["\\left\\lceil #1 \\right\\rceil", 1],
-        "ord": "\\operatorname{ord}",
-        "dlog": "\\operatorname{dlog}",
-        "lcm": "\\operatorname{lcm}",
-      }
+  TeX: {
+    Macros: {
+      "floor": ["\\left\\lfloor #1 \\right\\rfloor", 1],
+      "ceil": ["\\left\\lceil #1 \\right\\rceil", 1],
+      "setuni": "\\cup",
+      "setint": "\\cap",
+      "ord": "\\operatorname{ord}",
+      "dlog": "\\operatorname{dlog}",
+      "lcm": "\\operatorname{lcm}",
     }
+  }
 });
