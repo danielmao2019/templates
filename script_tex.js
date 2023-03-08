a321926c595ec7a308b804b84f3471b287377c54
danielmao2019 <danielmao2019@gmail.com>
2023-03-08 11:21:27 -0500

    updates

--- script_tex.js
+++ script_tex.js
@@ -5,9 +5,19 @@ MathJax.Hub.Config({
       "ceil": ["\\left\\lceil #1 \\right\\rceil", 1],
       "setuni": "\\cup",
       "setint": "\\cap",
+      "biguni": "\\bigcup",
+      "bigint": "\\bigcap",
       "ord": "\\operatorname{ord}",
       "dlog": "\\operatorname{dlog}",
       "lcm": "\\operatorname{lcm}",
+      // complexity
+      "P": "\\mathsf{P}",
+      "NP": "\\mathsf{NP}",
+      "EXP": "\\mathsf{EXP}",
+      "time": "\\mathsf{TIME}",
+      "space": "\\mathsf{SPACE}",
+      "pspace": "\\mathsf{PSPACE}",
+      "npspace": "\\mathsf{NPSPACE}",
     }
   }
 });
