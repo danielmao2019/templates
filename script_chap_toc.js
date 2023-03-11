e475d5255e6fb93378e7de6e09fd5c2fa4d2d59a
Daniel Mao <danielmao2019@gmail.com>
2023-03-11 15:39:58 -0500

    updates

--- script_chap_toc.js
+++ script_chap_toc.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() {
     const headings = document.querySelectorAll('h2, h3');
     var toc_list = document.createElement('ol');
@@ -28,3 +29,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('.chap-toc');
     container.appendChild(toc_list);
 });
+=======
+function generateTOC() {
+    var tocContainer = document.querySelector(".chap-toc");
+    var headings = document.querySelectorAll("h2");
+    var subheadings = document.querySelectorAll("h3");
+    var tocList = document.createElement("ol");
+    headings.forEach(function (heading) {
+        var tocListItem = document.createElement("li");
+        var tocLink = document.createElement("a");
+        tocLink.textContent = heading.textContent;
+        tocLink.href = "#" + heading.id;
+        tocListItem.appendChild(tocLink);
+        var subheadingElements = heading.nextElementSibling.querySelectorAll("h3");
+        if (subheadingElements.length > 0) {
+            var subList = document.createElement("ol");
+            subheadingElements.forEach(function (subheading) {
+                var subListItem = document.createElement("li");
+                var subLink = document.createElement("a");
+                subLink.textContent = subheading.textContent;
+                subLink.href = "#" + subheading.id;
+                subListItem.appendChild(subLink);
+                subList.appendChild(subListItem);
+            });
+            tocListItem.appendChild(subList);
+        }
+        tocList.appendChild(tocListItem);
+    });
+    tocContainer.appendChild(tocList);
+}
+
+document.addEventListener("DOMContentLoaded", generateTOC);
+>>>>>>> bca2f63 (updates)
