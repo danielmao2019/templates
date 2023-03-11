bca2f632fcc945e3847ea51725ede696bcb64f57
Daniel Mao <danielmao2019@gmail.com>
2023-03-11 15:39:58 -0500

    updates

--- script_chap_toc.js
+++ script_chap_toc.js
@@ -1,32 +1,30 @@
-// Define a function to generate the TOC
 function generateTOC() {
-  // Get the TOC container element
-  var tocContainer = document.querySelector(".chap-toc");
-
-  // Find all the h2 tags in the document
-  var headings = document.querySelectorAll("h2");
-
-  // Create an unordered list of links to the headings
-  var tocList = document.createElement("ol");
-  headings.forEach(function (heading) {
-    // Create a list item element
-    var tocListItem = document.createElement("li");
-
-    // Create an anchor element with a link to the heading ID
-    var tocLink = document.createElement("a");
-    tocLink.textContent = heading.textContent;
-    tocLink.href = "#" + heading.id;
-
-    // Add the anchor element to the list item element
-    tocListItem.appendChild(tocLink);
-
-    // Add the list item element to the TOC list
-    tocList.appendChild(tocListItem);
-  });
-
-  // Insert the TOC list into the TOC container
-  tocContainer.appendChild(tocList);
+    var tocContainer = document.querySelector(".chap-toc");
+    var headings = document.querySelectorAll("h2");
+    var subheadings = document.querySelectorAll("h3");
+    var tocList = document.createElement("ol");
+    headings.forEach(function (heading) {
+        var tocListItem = document.createElement("li");
+        var tocLink = document.createElement("a");
+        tocLink.textContent = heading.textContent;
+        tocLink.href = "#" + heading.id;
+        tocListItem.appendChild(tocLink);
+        var subheadingElements = heading.nextElementSibling.querySelectorAll("h3");
+        if (subheadingElements.length > 0) {
+            var subList = document.createElement("ol");
+            subheadingElements.forEach(function (subheading) {
+                var subListItem = document.createElement("li");
+                var subLink = document.createElement("a");
+                subLink.textContent = subheading.textContent;
+                subLink.href = "#" + subheading.id;
+                subListItem.appendChild(subLink);
+                subList.appendChild(subListItem);
+            });
+            tocListItem.appendChild(subList);
+        }
+        tocList.appendChild(tocListItem);
+    });
+    tocContainer.appendChild(tocList);
 }
 
-// Call the generateTOC function when the DOM is ready
 document.addEventListener("DOMContentLoaded", generateTOC);
